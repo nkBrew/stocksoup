9ef24d9d7de1e070d511ab0ec790bfe042bc0705
nkBrew <nicholasjwb@gmail.com>
2021-03-19 11:54:36 -0600

    Added more products

--- sites.py
+++ sites.py
@@ -1,23 +1,16 @@
-BEST_BUY = {
-    "https://www.bestbuy.ca/en-ca/product/logitech-logitech-g440-hard-polymer-gaming-mouse-pad-943-000049/10270597?icmp=Recos_4across_y_mght_ls_lk"
-    # "https://www.bestbuy.ca/en-ca/product/asus-tuf-gaming-nvidia-geforce-rtx-3080-10gb-gddr6x-video-card/14953248",
-    # "https://www.bestbuy.ca/en-ca/product/asus-rog-strix-nvidia-geforce-rtx-3080-10gb-gddr6x-video-card/14954116",
-    # "https://www.bestbuy.ca/en-ca/product/evga-geforce-rtx-3080-xc3-ultra-gaming-10gb-gddr6x-video-card/15084753",
-}
-
 BEST_BUY_STOCK = {
     # 'test': {
     #     'url': 'https://www.bestbuy.ca/en-ca/product/logitech-logitech-g440-hard-polymer-gaming-mouse-pad-943-000049/10270597',
     #     'sku': '10270597'
     # },
-    # 'https://www.bestbuy.ca/ecomm-api/availability/products?accept=application%2Fvnd.bestbuy.standardproduct.v1%2Bjson&accept-language=en-CA&skus=10270597': ,
+    # 'https://www.bestbuy.ca/ecomm-api/availability/products?accept=application%2Fvnd.bestbuy.standardproduct.v1%2Bjson&accept-language=en-CA&skus=10270597':
     'NVIDIA GeForce RTX 3070': {
         'url': 'https://www.bestbuy.ca/en-ca/product/nvidia-geforce-rtx-3070-8gb-gddr6-video-card-only-at-best-buy/15078017',
         'sku': '15078017',
     },
     'EVGA GeForce RTX 3070 XC3 Ultra': {
-        'url': 'https://www.bestbuy.ca/en-ca/product/nvidia-geforce-rtx-3070-8gb-gddr6-video-card-only-at-best-buy/15078017',
-        'sku': '15078017'
+        'url': 'https://www.bestbuy.ca/en-ca/product/evga-geforce-rtx-3070-xc3-ultra-8gb-gddr6-video-card/15147122',
+        'sku': '15147122'
     },
     'EVGA GeForce RTX 3080 XC3 Ultra': {
         'url': 'https://www.bestbuy.ca/en-ca/product/evga-geforce-rtx-3080-xc3-ultra-gaming-10gb-gddr6x-video-card/15084753',
@@ -31,6 +24,26 @@ BEST_BUY_STOCK = {
         'url': 'https://www.bestbuy.ca/en-ca/product/asus-rog-strix-nvidia-geforce-rtx-3080-10gb-gddr6x-video-card/14954116',
         'sku': '14954116'
     },
+    'ASUS ROG Strix NVIDIA GeForce RTX 3090': {
+        'url': 'https://www.bestbuy.ca/en-ca/product/asus-rog-strix-nvidia-geforce-rtx-3090-24gb-gddr6x-video-card/14954117',
+        'sku': '14954117'
+    },
+    'ASUS TUF Gaming NVIDIA GeForce RTX 3090':{
+        'url': 'https://www.bestbuy.ca/en-ca/product/asus-tuf-gaming-nvidia-geforce-rtx-3090-24gb-gddr6x-video-card/14953247',
+        'sku': '14953247'
+    },
+    'ZOTAC NVIDIA GeForce RTX 3090':{
+        'url': 'https://www.bestbuy.ca/en-ca/product/zotac-nvidia-geforce-rtx-3090-trinity-24gb-gddr6x-video-card/14953250',
+        'sku': '14953250'
+    },
+    'EVGA NVIDIA GeForce RTX 3090':{
+        'url': 'https://www.bestbuy.ca/en-ca/product/evga-nvidia-geforce-rtx-3090-xc3-ultra-gaming-24gb-gddr6x-video-card/14967857',
+        'sku': '14967857'
+    },
+    'MSI NVIDIA GeForce RTX 3090 VENTUS 3X': {
+        'url': 'https://www.bestbuy.ca/en-ca/product/msi-nvidia-geforce-rtx-3090-ventus-3x-oc-24gb-gddr6x-video-card/14966477',
+        'sku': '14966477'
+    }
 }
 
 BEST_BUY_ECOMM_URL = 'https://www.bestbuy.ca/ecomm-api/availability/products?accept=application%2Fvnd.bestbuy.standardproduct.v1%2Bjson&accept-language=en-CA&skus='
